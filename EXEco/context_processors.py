178af032d779c338680b39a4b416afdfee3763ba
dari <dariushtomlinson@gmail.com>
2025-02-13 01:54:04 +0000

    try to fix test failure

--- EXEco/context_processors.py
+++ EXEco/context_processors.py
@@ -4,7 +4,12 @@ from accounts.models import CustomUser
 def user_context(request):
     if request.user.is_authenticated:
         user = CustomUser.objects.get(id=request.user.id)
-        user.initials = user.first_name[0] + user.last_name[0]
-        return {"fullUser": user}
-    else:
-        return {"fullUser", None}
+        return {
+            "fullUser": {
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "initials": (user.first_name[:1] + user.last_name[:1]).upper()
+            }
+        }
+    return {}
