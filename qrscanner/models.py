b20f0b16c05bc3c08ba013bf02f182fb9b674544
W3r5l3y <102gaminginbox@gmail.com>
2025-02-17 22:05:29 +0000

    Add Location model

--- qrscanner/models.py
+++ qrscanner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    location_code = models.CharField(max_length=4, primary_key=True)
+    location_name = models.CharField(max_length=255)
+    location_fact = models.TextField()
+    cooldown_length = models.IntegerField()  # Cooldown is in minutes
+    times_visited = models.IntegerField(default=0)  # Tracks number of scans/visits
+
+    def __str__(self):
+        return f"{self.location_name} ({self.location_code})"
