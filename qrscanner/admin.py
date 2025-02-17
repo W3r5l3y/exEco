af86a5401b8b185e06f606fe05f5314f41bd88a6
W3r5l3y <102gaminginbox@gmail.com>
2025-02-17 22:05:21 +0000

    Add permissions for admin to edit locations table

--- qrscanner/admin.py
+++ qrscanner/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "location_code",
+        "location_name",
+        "cooldown_length",
+        "times_visited",
+    )
+    search_fields = ("location_code", "location_name")
+    list_filter = ("cooldown_length",)
