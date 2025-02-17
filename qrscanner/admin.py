0a581491d441a7b1bd3a4d6b6b53e77a317e414a
W3r5l3y <102gaminginbox@gmail.com>
2025-02-17 23:08:22 +0000

    Add permission for admin to edit and add tuples to database

--- qrscanner/admin.py
+++ qrscanner/admin.py
@@ -2,13 +2,21 @@ from django.contrib import admin
 from .models import Location
 
 
-@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = (
         "location_code",
         "location_name",
+        "location_value",
         "cooldown_length",
         "times_visited",
     )
     search_fields = ("location_code", "location_name")
-    list_filter = ("cooldown_length",)
+    list_editable = (
+        "location_name",
+        "location_value",
+        "cooldown_length",
+        "times_visited",
+    )
+
+
+admin.site.register(Location, LocationAdmin)
