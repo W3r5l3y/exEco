2f8eb303b2a4205e2b9bcd5ecaf708a6681760a6
W3r5l3y <102gaminginbox@gmail.com>
2025-02-17 21:28:46 +0000

    Add QR code scanning functionality with image upload form

---
+++ qrscanner/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class QRCodeUploadForm(forms.Form):
+    image = forms.ImageField()
--- qrscanner/views.py
+++ qrscanner/views.py
@@ -1,3 +1,26 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
 from django.shortcuts import render
+from .forms import QRCodeUploadForm
 
-# Create your views here.
+
+def scan_qr(request):
+    result = None
+    if request.method == "POST":
+        form = QRCodeUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data["image"]
+            image_array = np.asarray(bytearray(image.read()), dtype=np.uint8)
+            img = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+
+            decoded_objects = decode(img)
+            if decoded_objects:
+                result = decoded_objects[0].data.decode(
+                    "utf-8"
+                )  # Extract string from QR code
+
+    else:
+        form = QRCodeUploadForm()
+
+    return render(request, "qrscanner/scan_qr.html", {"form": form, "result": result})
