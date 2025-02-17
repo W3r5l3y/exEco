e6a5430ba19cc4c013c5c342398e1f2c7932b5a8
W3r5l3y <102gaminginbox@gmail.com>
2025-02-17 22:05:36 +0000

    Add location tracking to QR code scanning

--- qrscanner/views.py
+++ qrscanner/views.py
@@ -3,10 +3,13 @@ import numpy as np
 from pyzbar.pyzbar import decode
 from django.shortcuts import render
 from .forms import QRCodeUploadForm
+from .models import Location
 
 
 def scan_qr(request):
     result = None
+    location = None
+
     if request.method == "POST":
         # If request method is POST, get the form data
         form = QRCodeUploadForm(request.POST, request.FILES)
@@ -22,10 +25,21 @@ def scan_qr(request):
                     "utf-8"
                 )  # Extracted string from QR code
                 print(result)  # TODO remove after debugging
+                try:
+                    location = Location.objects.get(location_code=result)
+                    location.times_visited += 1
+                    location.save()
+                    print(location)  # TODO remove after debugging
+                except Location.DoesNotExist:
+                    result = "Location not found for code: " + result
 
     else:
         # If request method is not POST, create a new form
         form = QRCodeUploadForm()
 
     # Render the template with the form and result
-    return render(request, "qrscanner/scan_qr.html", {"form": form, "result": result})
+    return render(
+        request,
+        "qrscanner/scan_qr.html",
+        {"form": form, "result": result, "location": location},
+    )
