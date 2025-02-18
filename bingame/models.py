09ccea420c18057240af5e415a841fe6dea40850
henrydvies <85992819+henrydvies@users.noreply.github.com>
2025-02-18 14:25:17 +0000

    Adding the Items, and Bins models

--- bingame/models.py
+++ bingame/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# The table for items
+class Items(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=100)
+    item_image = models.ImageField(upload_to="static/img/items/")
+    bin_id = models.ForeignKey(Bins, on_delete=models.CASCADE)
+    
+# The table for bins
+class Bins(models.Model):
+    bin_id = models.AutoField(primary_key=True)
+    bin_name = models.CharField(max_length=100)
+    bin_image = models.ImageField(upload_to="static/img/bins/")
