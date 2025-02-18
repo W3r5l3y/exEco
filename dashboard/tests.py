101199f9db2d383634197a75449e37a017acb285
W3r5l3y <102gaminginbox@gmail.com>
2025-02-18 01:00:43 +0000

    Update tests to reflect change in automatic redirections

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,8 +1,19 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.templatetags.static import static
 
 
 class DashboardViewTestCase(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            email="test@example.com",
+            first_name="Test",
+            last_name="User",
+            password="password123",
+        )
+        self.client.login(email="test@example.com", password="password123")
+
     def test_dashboard_view_get(self):
         response = self.client.get(reverse("dashboard"))
         self.assertEqual(response.status_code, 200)
@@ -10,7 +21,8 @@ class DashboardViewTestCase(TestCase):
 
     def test_dashboard_view_contains_banner(self):
         response = self.client.get(reverse("dashboard"))
-        self.assertContains(response, 'src="/static/img/dashboard-banner.jpg"')
+        banner_url = static("img/dashboard-banner.jpg")
+        self.assertContains(response, f'src="{banner_url}"')
 
     def test_dashboard_view_contains_game_cards(self):
         response = self.client.get(reverse("dashboard"))
