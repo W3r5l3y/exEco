3468dfad1a4402e8aeaa31e1bbd599d96bbb8c5e
Liqem <liamtadhunter@gmail.com>
2025-02-21 15:46:11 +0000

    Add Chip Packet item to bins and include corresponding image

--- bingame/migrations/0002_populate_bins_and_items.py
+++ bingame/migrations/0002_populate_bins_and_items.py
@@ -29,6 +29,7 @@ def populate_bins_and_items(apps, schema_editor):
         {'item_name': 'Cardboard', 'item_image': 'static/img/items/cardboard.jpg', 'bin_id': 3},
         {'item_name': 'Tin', 'item_image': 'static/img/items/tins.jpg', 'bin_id': 4},
         {'item_name': 'Glass Bottle', 'item_image': 'static/img/items/glass_bottle.png', 'bin_id': 2},
+        {'item_name': 'Chip Packet', 'item_image': 'static/img/items/chip_packet.png', 'bin_id': 5},
     ]
 
     for item_entry in item_data:
