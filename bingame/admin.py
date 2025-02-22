7dd23447900faaf7c8567e5cc6ee22bd6d650aa0
henrydvies <85992819+henrydvies@users.noreply.github.com>
2025-02-22 18:47:25 +0000

    FIXING

--- bingame/admin.py
+++ bingame/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bins, Items, BinLeaderboardEntry
+from .models import Bins, Items
 
 # Register your models here.
 class BinsAdmin(admin.ModelAdmin):
