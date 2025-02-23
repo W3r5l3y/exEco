13a63df6e2a4a232fb1a96985bda8e665cc870e5
W3r5l3y <102gaminginbox@gmail.com>
2025-02-23 11:57:50 +0000

    Refactor code for improved readability and PEP8 compliance

--- bingame/apps.py
+++ bingame/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class BingameConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bingame'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "bingame"
