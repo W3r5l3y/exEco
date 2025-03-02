0342cb350ad9315647fda8a0218e72d97d1c0c71
sfmcmullen <sfmcmullen@gmail.com>
2025-03-02 22:57:22 +0000

    Add load inventory URL path to garden app

--- garden/urls.py
+++ garden/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import garden_view, save_garden, load_garden
+from .views import garden_view, save_garden, load_garden, load_inventory
 
 urlpatterns = [
     path("garden/", garden_view, name="garden"),
     path("save-garden/", save_garden, name="save_garden"),
     path("load-garden/", load_garden, name="load_garden"),
+    path("load-inventory/", load_inventory, name="load_inventory"),
 ]
