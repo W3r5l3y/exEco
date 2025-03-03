8bd6f6c09b377a3b86e3ba37fdab00993e7c3227
sfmcmullen <sfmcmullen@gmail.com>
2025-03-03 00:06:58 +0000

    Add URL pattern for fetching user's garden image

--- garden/urls.py
+++ garden/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import garden_view, save_garden, load_garden, load_inventory, save_garden_as_image
+from .views import garden_view, save_garden, load_garden, load_inventory, save_garden_as_image, fetch_user_garden_image
 
 urlpatterns = [
     path("garden/", garden_view, name="garden"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("load-garden/", load_garden, name="load_garden"),
     path("load-inventory/", load_inventory, name="load_inventory"),
     path("save-garden-as-image/", save_garden_as_image, name="save_garden_as_image"),
+    path("fetch-user-garden-image/", fetch_user_garden_image, name="fetch_user_garden_image"),
 ]
