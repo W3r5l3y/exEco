da76a39bb7254a01984e22a76fc935b87b0746bc
sfmcmullen <sfmcmullen@gmail.com>
2025-03-06 22:33:44 +0000

    Add get_lootbox_data endpoint to bingame URLs

--- bingame/urls.py
+++ bingame/urls.py
@@ -1,19 +1,11 @@
 from django.urls import path
-from .views import (
-    game_view,
-    update_leaderboard,
-    fetch_random_items,
-    get_bingame_leaderboard,
-)
+from .views import (game_view, update_leaderboard, fetch_random_items, get_bingame_leaderboard, get_lootbox_data,)
 
 
 urlpatterns = [
     path("bingame/", game_view, name="bingame"),
-    path("update-leaderboard/", update_leaderboard, name="update-leaderboard"),
-    path(
-        "get-bingame-leaderboard/",
-        get_bingame_leaderboard,
-        name="get_bingame_leaderboard",
-    ),
-    path("fetch_random_items/", fetch_random_items, name="fetch_random_items"),
+    path("update-leaderboard/", update_leaderboard, name="update_leaderboard"),
+    path("get-bingame-leaderboard/", get_bingame_leaderboard, name="get_bingame_leaderboard"),
+    path("fetch-random-items/", fetch_random_items, name="fetch_random_items"),
+    path("get-lootbox-data/", get_lootbox_data, name="get_lootbox_data"),
 ]
