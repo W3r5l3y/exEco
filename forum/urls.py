8f61c779fd996eb16ee8fcc3e3aa057809cbc8ca
W3r5l3y <102gaminginbox@gmail.com>
2025-03-07 13:57:50 +0000

    Add user profile URL pattern to access user profiles

--- forum/urls.py
+++ forum/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import forum_home, create_post, like_post, report_post
+from .views import forum_home, create_post, like_post, report_post, user_profile
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path("create/", create_post, name="create_post"),
     path("like/<int:post_id>/", like_post, name="like_post"),
     path("report/<int:post_id>/", report_post, name="report_post"),
+    path("user/<int:user_id>/", user_profile, name="user_profile"),
 ]
 
 if settings.DEBUG:
