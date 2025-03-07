ac0bdbcefb19959a03cfbe445b1fd635da1af747
dari <dariushtomlinson@gmail.com>
2025-03-07 21:01:07 +0000

    minor changes

--- forum/static/js/user_profile.js
+++ forum/static/js/user_profile.js
@@ -5,26 +5,26 @@ function sharePost(postId) {
         });
     }
 
-function toggleComments(postId) {
-    const commentsContainer = document.getElementById(`comments-${postId}`);
-    commentsContainer.style.display = commentsContainer.style.display === 'none' ? 'block' : 'none';
-}
+    function toggleComments(postId) {
+        const commentsContainer = document.getElementById(`comments-${postId}`);
+        commentsContainer.style.display = commentsContainer.style.display === 'none' ? 'block' : 'none';
+    }
 
-function likePost(postId) {
-    fetch(`{% url 'like_post' 0 %}`.replace('0', postId), {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': '{{ csrf_token }}',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({})
-    })
-    .then(response => response.json())
-    .then(data => {
-        const likeCountText = document.querySelector(`#like-count-${postId}`);
-        likeCountText.textContent = data.likes;
-        const likeButton = document.querySelector(`#like-button-${postId}`);
-        likeButton.src = data.liked ? '{% static "img/liked.svg" %}' : '{% static "img/like.svg" %}';
-    })
-    .catch(error => console.error('Error:', error));
-}
+    function likePost(postId) {
+        fetch(`{% url 'like_post' 0 %}`.replace('0', postId), {
+            method: 'POST',
+            headers: {
+                'X-CSRFToken': '{{ csrf_token }}',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({})
+        })
+        .then(response => response.json())
+        .then(data => {
+            const likeCountText = document.querySelector(`#like-count-${postId}`);
+            likeCountText.textContent = data.likes;
+            const likeButton = document.querySelector(`#like-button-${postId}`);
+            likeButton.src = data.liked ? '{% static "img/liked.svg" %}' : '{% static "img/like.svg" %}';
+        })
+        .catch(error => console.error('Error:', error));
+    }
