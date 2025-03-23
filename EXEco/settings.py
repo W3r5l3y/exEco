f7925b288175f597ae72f72a1ed0bbaf13bc0262
W3r5l3y <102gaminginbox@gmail.com>
2025-03-23 21:24:09 +0000

    Add CSRF trusted origins for production environment

--- EXEco/settings.py
+++ EXEco/settings.py
@@ -181,3 +181,8 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    "https://execo-production.up.railway.app/",
+]
